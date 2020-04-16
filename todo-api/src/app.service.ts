100b537baf092db32d10bf828c7746913ba02b7d
Taylor Buckner <taylora.buckner@gmail.com>
2020-04-16 08:17:11 -0500

    feat(api): stub out crud operations.

--- todo-api/src/app.service.ts
+++ todo-api/src/app.service.ts
@@ -6,4 +6,21 @@ export class AppService {
   getToDoItems(): Array<string> {
     return [...MOCK_TODO_ITEMS];
   }
+
+  public getToDoItem(id: number): string {
+    return MOCK_TODO_ITEMS[id + 1];
+  }
+
+  public addToDoItem(toDoItem: string): void {
+    MOCK_TODO_ITEMS.push(toDoItem);
+  }
+
+  public updateToDoItem(toDoItem: string) {
+    // Update.
+    // NOTE: Might need the whole model.
+  }
+
+  public removeToDoItem(id: number): void {
+    // Remove.
+  }
 }
