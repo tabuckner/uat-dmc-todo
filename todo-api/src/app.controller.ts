3d462f00fffcd2f28cc517102b5d78f505aafde0
Taylor Buckner <taylora.buckner@gmail.com>
2020-04-16 10:54:07 -0500

    feat(api): add SQL statements to app.service.

--- todo-api/src/app.controller.ts
+++ todo-api/src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ToDoItemModel } from '../../lib/models/todo-item.model';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getToDos(): Array<string> {
+  public async getToDos(): Promise<Array<ToDoItemModel>> {
     return this.appService.getToDoItems();
   }
 }
--- todo-api/src/app.service.ts
+++ todo-api/src/app.service.ts
@@ -1,26 +1,37 @@
-import { Injectable } from '@nestjs/common';
-import { MOCK_TODO_ITEMS } from './constants/mocks/todo-items';
+import { Injectable, Inject } from '@nestjs/common';
+import { Client } from 'pg';
+import { ToDoItemModel } from '../../lib/models/todo-item.model'
+import { ToDoItemDto } from '../../lib/models/todo-item.dto';
 
 @Injectable()
 export class AppService {
-  getToDoItems(): Array<string> {
-    return [...MOCK_TODO_ITEMS];
+
+  constructor(@Inject('DATABASE_CONNECTION') private connectedClient: Client) {}
+
+  public async getToDoItems(): Promise<Array<ToDoItemModel>> {
+    const query = 'SELECT * FROM app_schema.todo_items';
+    const result = await this.connectedClient.query(query);
+    return result.rows;
   }
 
-  public getToDoItem(id: number): string {
-    return MOCK_TODO_ITEMS[id + 1];
+  public async getToDoItem(id: number): Promise<Array<ToDoItemModel>> {
+    const query = 'SELECT * FROM app_schema.todo_items WHERE id = $1';
+    const result = await this.connectedClient.query(query, [id]);
+    return result.rows;
   }
 
-  public addToDoItem(toDoItem: string): void {
-    MOCK_TODO_ITEMS.push(toDoItem);
+  public async addToDoItem(newToDoItem: ToDoItemDto): Promise<void> {
+    const query = 'INSERT INTO app_schema.todo_items (description) VALUES ($1)';
+    await this.connectedClient.query(query, [newToDoItem.description]);
   }
 
-  public updateToDoItem(toDoItem: string) {
-    // Update.
-    // NOTE: Might need the whole model.
+  public async updateToDoItem(id: number, nextToDoItem: ToDoItemDto): Promise<void> {
+    const query = 'UPDATE app_schema.todo_items SET description = $2 WHERE id = $1';
+    await this.connectedClient.query(query, [id, nextToDoItem.description]);
   }
 
-  public removeToDoItem(id: number): void {
-    // Remove.
+  public async removeToDoItem(id: number): Promise<void> {
+    const query = 'DELETE FROM app_schema.todo_items WHERE id = $1'
+    await this.connectedClient.query(query, [id]);
   }
 }
